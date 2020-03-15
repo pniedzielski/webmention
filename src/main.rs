53ebd9a2fa5e189be292390095a4ecb208beb82d
Patrick M. Niedzielski <patrick@pniedzielski.net>
2020-03-15 22:28:10 +0000

    Add URL format validation
    This patch adds validation of the URL structure using the `url`
    package and its parsing capability.
    
    Doing this is more complex than one might like, because the `Url` type
    does not implement Serde's `Deserialize` trait, so we have to manually
    construct a visitor that forwards to `Url::parse` (and which traffics
    in the correct error type).
    
    Testing
    -------
    
    This patch was tested by passing ill-formed URLs to both the `source`
    and `target` parameters.
    
    License: GPLv3+
    Signed-off-by: Patrick M. Niedzielski <patrick@pniedzielski.net>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
 use actix_web::{web, App, HttpResponse, HttpServer, post};
-use serde::Deserialize;
+use serde::{de,Deserialize};
+use std::fmt;
+use url::Url;
 
 #[derive(Deserialize)]
 struct Webmention {
-    source: String,
-    target: String,
+    #[serde(deserialize_with = "deserialize_url")]
+    source: Url,
+    #[serde(deserialize_with = "deserialize_url")]
+    target: Url,
+}
+
+pub fn deserialize_url<'de, D>(d: D) -> Result<Url, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    d.deserialize_str(UrlVisitor)
+}
+
+struct UrlVisitor;
+
+impl<'de> de::Visitor<'de> for UrlVisitor {
+    type Value = Url;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a URL")
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        match Url::parse(value) {
+            Ok(url) => Ok(url),
+            Err(e) => Err(E::custom(
+                format!("Parse error {} for {}", e, value)
+            )),
+        }
+    }
 }
 
 #[post("/")]
