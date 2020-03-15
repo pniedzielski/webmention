a60b688e00702b7ed3bc45232f7e00ff5f935f0a
Patrick M. Niedzielski <patrick@pniedzielski.net>
2020-03-15 21:02:23 +0000

    Import a basic Actix web server
    This patch adds a basic [Actix][actix] example web server, taken from
    the [Actix documentation][source].
    
    [actix]:  https://actix.rs/
    [source]: https://actix.rs/docs/getting-started/
    
    License: GPLv3+
    Signed-off-by: Patrick M. Niedzielski <patrick@pniedzielski.net>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, get};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!\n")
+}
+
+async fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!\n")
+}
+
+#[get("/hello")]
+async fn index3() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!\n")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index2))
+            .service(index3)
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
